187d82b548fe1471f7146776679b28ae24e17c9c
nicoleslin <nsl.nicoleslin@gmail.com>
2017-11-12 04:22:21 -0500

    added wigglesIntent and whoopsIntent
    fruit salad, yummy yummy

--- alexa/NutritionCalculator.py
+++ alexa/NutritionCalculator.py
@@ -4,59 +4,77 @@ from scipy.spatial import distance
 from itertools import combinations
 import re
 
-data = pd.read_csv("/Users/nicolelin/Documents/GitHub/KnowFood/" + str(location) + str(meal) + ".csv", encoding = 'latin-1')
 h = 160
 w = 80
 g = "Male"
 age = 18
 activity = 3
+dietRestriction = "None"
 
 if g is "Male":
-    calories = (10*w+6.25*h-5*age+5)*1.2+300*activity
+    calories = (int)(((10 * w + 6.25 * h - 5 * age + 5) * 1.2 + 300 * activity) / 3)
 else:
-    calories = (10*w+6.25*h-5*age-161)*1.2+300*activity
+    calories = (int)(((10 * w + 6.25 * h - 5 * age - 161) * 1.2 + 300 * activity) / 3)
 
-tFat = 60*calories/2000
-tCholesterol = 290*calories/2000
-tSodium = 2350*calories/2000
-tCarbs = 320*calories/2000
-tProtein = 0.8*w
+tFat = 70 * calories / 2000
+tCholesterol = 290 * calories / 2000
+tSodium = 2350 * calories / 2000
+tCarbs = 320 * calories / 2000
+tProtein = 0.8 * w / 3
 tMatrix = np.array([tFat, tCholesterol, tSodium, tCarbs, tProtein])
 
-rows = np.arange(6)
-nums = np.array([0,0,0,0])
-cases = nums
+data = pd.read_csv("/Users/harry/Downloads/out.csv", encoding='latin-1')
+filt = pd.read_csv("/Users/harry/Downloads/KnowFood-master/WilcoxLunch.csv", encoding='latin-1')
+# dietaryFilter
+try:
+    other = data[data['Label'].isin(filt['Other'])]
+except ValueError:
+    other = pd.DataFrame({'Other': []})
+try:
+    vegetarian = data[data['Label'].isin(filt['Vegetarian'])]
+except ValueError:
+    vegetarian = pd.DataFrame({'Vegetarian': []})
+try:
+    vegan = data[data['Label'].isin(filt['Vegan'])]
+except ValueError:
+    vegan = pd.DataFrame({'Vegan': []})
+try:
+    pork = data[data['Label'].isin(filt['Pork'])]
+except ValueError:
+    pork = pd.DataFrame({'Pork': []})
 
-ideal = np.array([0,0,0,0])
-idealFoods = np.array([0,0,0,0])
+if dietRestriction is "None":
+    selection = pd.concat([vegetarian, vegan, pork, other], axis=0)
+elif dietRestriction is "Vegan":
+    selection = vegan
+elif dietRestriction is "Vegetarian":
+    selection = pd.concat([vegetarian, vegan], axis=0)
+else:
+    selection = pd.concat([vegetarian, vegan, other], axis=0)
 
+selection = selection.drop_duplicates(subset='Label', keep='first')
+
+rows = np.arange(selection.shape[0])
+cases = np.array([10000000, 10000000, 10000000, 10000000])
 
 for p in combinations(rows, 4):
-    minimum = float('inf')
-    row_0 = data.iloc[p[0]]
+    cases = np.array([10000000,10000000,10000000,10000000])
+    row_0 = selection.iloc[p[0]]
     calories_0 = (int)(row_0['Calories'])
-    row_1 = data.iloc[p[1]]
+    row_1 = selection.iloc[p[1]]
     calories_1 = (int)(row_1['Calories'])
-    row_2 = data.iloc[p[2]]
+    row_2 = selection.iloc[p[2]]
     calories_2 = (int)(row_2['Calories'])
-    row_3 = data.iloc[p[3]]
+    row_3 = selection.iloc[p[3]]
     calories_3 = (int)(row_3['Calories'])
-    while nums[0]*calories_0 < calories:
-        nums[0] = nums[0]+1
-        while nums[0]*calories_0 + nums[1]*calories_1 < calories:
-            nums[1] = nums[1]+1
-            while nums[0]*calories_0 + nums[1]*calories_1 + nums[2]*calories_2< calories:
-                nums[2] = nums[2]+1
-                while nums[0]*calories_0 + nums[1]*calories_1 + nums[2]*calories_2 + nums[3]*calories_3 < calories:
-                    nums[3] = nums[3]+1
-                cases = np.vstack((cases, nums))
-                nums[3] = 0
-            cases = np.vstack((cases, nums))
-            nums[2] = 0
-        cases = np.vstack((cases, nums))
-        nums[1] = 0
-    cases = np.vstack((cases, nums))
-    nums[0] = 0
+
+    for i in range(20):
+        cases = np.vstack((cases, [np.random.randint(15, 35)/100*calories/calories_0,
+                                   np.random.randint(15, 35)/100*calories/calories_1,
+                                   np.random.randint(15, 35)/100*calories/calories_2,
+                                   np.random.randint(15, 35)/100*calories/calories_3]))
+
+    minimum = float('inf')
     oFat_0 = (float)(row_0['Fat'])
     oFat_1 = (float)(row_1['Fat'])
     oFat_2 = (float)(row_2['Fat'])
@@ -77,31 +95,31 @@ for p in combinations(rows, 4):
     oProtein_1 = (float)(row_1['Protein'])
     oProtein_2 = (float)(row_2['Protein'])
     oProtein_3 = (float)(row_3['Protein'])
-    for i in range(0, cases.shape[0]-1):
+
+    for i in range(21):
         oMatrix = np.array([oFat_0*cases[i, 0]+oFat_1*cases[i, 1]+oFat_2*cases[i, 2]+oFat_3*cases[i, 3],
-                           oCholesterol_0*cases[i, 0]+oCholesterol_1*cases[i, 1]+oCholesterol_2*cases[i, 2]+oCholesterol_3*cases[i, 3],
-                           oSodium_0*cases[i, 0]+oSodium_1*cases[i, 1]+oSodium_2*cases[i, 2]+oSodium_3*cases[i, 3],
-                           oCarbs_0*cases[i, 0]+oCarbs_1*cases[i, 1]+oCarbs_2*cases[i, 2]+oCarbs_3*cases[i, 3],
-                           oProtein_0*cases[i, 0]+oProtein_1*cases[i, 1]+oProtein_2*cases[i, 2]+oProtein_3*cases[i, 3]])
+                            oCholesterol_0*cases[i, 0]+oCholesterol_1*cases[i, 1]+oCholesterol_2*cases[i, 2]+oCholesterol_3*cases[i, 3],
+                            oSodium_0*cases[i, 0]+oSodium_1*cases[i, 1]+oSodium_2*cases[i, 2]+oSodium_3*cases[i, 3],
+                            oCarbs_0*cases[i, 0]+oCarbs_1*cases[i, 1]+oCarbs_2*cases[i, 2]+oCarbs_3*cases[i, 3],
+                            oProtein_0*cases[i, 0]+oProtein_1*cases[i, 1]+oProtein_2*cases[i, 2]+oProtein_3*cases[i, 3]])
         if distance.euclidean(tMatrix, oMatrix) < minimum:
             minimum = distance.euclidean(tMatrix, oMatrix)
             ideal = cases[i]
             idealFoods = p
-
-food1 = data.iloc[idealFoods[0]]
-food2 = data.iloc[idealFoods[1]]
-food3 = data.iloc[idealFoods[2]]
-food4 = data.iloc[idealFoods[3]]
+food1 = selection.iloc[idealFoods[0]]
+food2 = selection.iloc[idealFoods[1]]
+food3 = selection.iloc[idealFoods[2]]
+food4 = selection.iloc[idealFoods[3]]
 
 label1 = food1['Label']
 label2 = food2['Label']
 label3 = food3['Label']
 label4 = food4['Label']
 
-amount1 = (float)(re.findall(r'[\d\.\d]+', food1['Serving Size'])[0])*ideal[0]
-amount2 = (float)(re.findall(r'[\d\.\d]+', food2['Serving Size'])[0])*ideal[1]
-amount3 = (float)(re.findall(r'[\d\.\d]+', food3['Serving Size'])[0])*ideal[2]
-amount4 = (float)(re.findall(r'[\d\.\d]+', food4['Serving Size'])[0])*ideal[3]
+amount1 = np.round((float)(re.findall(r'[\d\.\d]+', food1['Serving Size'])[0])*ideal[0])
+amount2 = np.round((float)(re.findall(r'[\d\.\d]+', food2['Serving Size'])[0])*ideal[1])
+amount3 = np.round((float)(re.findall(r'[\d\.\d]+', food3['Serving Size'])[0])*ideal[2])
+amount4 = np.round((float)(re.findall(r'[\d\.\d]+', food4['Serving Size'])[0])*ideal[3])
 
 unit1 = food1['Serving Size'].lstrip('0123456789.- ')
 unit2 = food2['Serving Size'].lstrip('0123456789.- ')
--- alexa/alexa_know_food.py
+++ alexa/alexa_know_food.py
@@ -11,30 +11,35 @@ import re
 app = Flask(__name__)
 ask = Ask(app, "/")
 
-@ask.intent("CreateNewUser", convert={'height': int, 'weight': int, 'age': int, 'activity': int})
-def setCalories(height, weight, age, activity, gender):
-    global calories
+#@ask.intent("CreateNewUser", convert={'height': int, 'weight': int, 'age': int, 'activity': int})
+#def setCalories(height, weight, age, activity, gender):
+#    global calories
 
-    if gender is "Male":
-        calories = (10*weight+6.25*height-5*age+5)*1.2+300*activity
-    else:
-        calories = (10*weight+6.25*height-5*age-161)*1.2+300*activity
+height = 420
+weight = 800
+gender = "Male"
+age = 18
+activity = 3
+dietRestriction = "None"
 
-    tFat = 60*calories/2000
-    tCholesterol = 290*calories/2000
-    tSodium = 2350*calories/2000
-    tCarbs = 320*calories/2000
-    tProtein = 0.8*weight
-    global tMatrix
-    tMatrix = np.array([tFat, tCholesterol, tSodium, tCarbs, tProtein])
+if gender is "Male":
+    calories = (int)(((10 * weight + 6.25 * height - 5 * age + 5) * 1.2 + 300 * activity) / 3)
+else:
+    calories = (int)(((10 * weight + 6.25 * height - 5 * age - 161) * 1.2 + 300 * activity) / 3)
 
-    rows = np.arange(6)
-    nums = np.array([0,0,0,0])
-    cases = nums
+tFat = 70 * calories / 2000
+tCholesterol = 290 * calories / 2000
+tSodium = 2350 * calories / 2000
+tCarbs = 320 * calories / 2000
+tProtein = 0.8 * weight / 3
+tMatrix = np.array([tFat, tCholesterol, tSodium, tCarbs, tProtein])
 
-    ideal = np.array([0,0,0,0])
-    idealFoods = np.array([0,0,0,0])
-
+#def clarify(loc):
+#    CJL = ["see jay L", "see jay l", "see jay el", "cjl", "the center for Jewish life", "center for Jewish life"]
+#    butlerwilson = ["butler wilson, "]
+#    roma = ["rockymattie dining hall", "rockymattie", "rocky Maddy", ""]
+#    whitman = []
+#    return location
 
 # Optional
 @app.route('/')
@@ -49,41 +54,69 @@ def start_skill():
 @ask.intent("YesIntent")
 @ask.intent("RequestMealPlan")
 def suggested_meal(location, meal):
-    data = pd.read_csv("/Users/nicolelin/Documents/GitHub/KnowFood/" + str(location) + str(meal) + ".csv", encoding = 'latin-1')
-    if (location is None):
-        reprompt_location = "i didn't catch where you wanted to eat. where would you like to go?"
-        return question(reprompt_location)
+    #data = pd.read_csv("/Users/nicolelin/Documents/GitHub/KnowFood/" + str(location) + str(meal) + ".csv", encoding = 'latin-1')
+    #if (location is None):
+    #    reprompt_location = "i didn't catch where you wanted to eat. where would you like to go?"
+    #    return question(reprompt_location)
 
-    if (meal is None):
-        reprompt_meal = "i can show you options for lunch or dinner. what meal would you like to eat?"
-        return question(reprompt_meal)
+    #if (meal is None):
+    #    reprompt_meal = "i can show you options for lunch or dinner. what meal would you like to eat?"
+    #    return question(reprompt_meal)
+    #loc = clarify(location)
+
+    data = pd.read_csv("/Users/nicolelin/Documents/GitHub/KnowFood/out.csv", encoding='latin-1')
+    filt = pd.read_csv("/Users/nicolelin/Documents/GitHub/KnowFood/" + location + meal + ".csv", encoding='latin-1')
+    #filt = pd.read_csv("/Users/nicolelin/Documents/GitHub/KnowFood/" + str(location) + str(meal) + ".csv", encoding='latin-1')
+    # dietaryFilter
+    try:
+        other = data[data['Label'].isin(filt['Other'])]
+    except ValueError:
+        other = pd.DataFrame({'Other': []})
+    try:
+        vegetarian = data[data['Label'].isin(filt['Vegetarian'])]
+    except ValueError:
+        vegetarian = pd.DataFrame({'Vegetarian': []})
+    try:
+        vegan = data[data['Label'].isin(filt['Vegan'])]
+    except ValueError:
+        vegan = pd.DataFrame({'Vegan': []})
+    try:
+        pork = data[data['Label'].isin(filt['Pork'])]
+    except ValueError:
+        pork = pd.DataFrame({'Pork': []})
+
+    if dietRestriction is "None":
+        selection = pd.concat([vegetarian, vegan, pork, other], axis=0)
+    elif dietRestriction is "Vegan":
+        selection = vegan
+    elif dietRestriction is "Vegetarian":
+        selection = pd.concat([vegetarian, vegan], axis=0)
+    else:
+        selection = pd.concat([vegetarian, vegan, other], axis=0)
+
+    selection = selection.drop_duplicates(subset='Label', keep='first')
+
+    rows = np.arange(selection.shape[0])
+    cases = np.array([10000000, 10000000, 10000000, 10000000])
 
     for p in combinations(rows, 4):
-        minimum = float('inf')
-        row_0 = data.iloc[p[0]]
+        cases = np.array([10000000,10000000,10000000,10000000])
+        row_0 = selection.iloc[p[0]]
         calories_0 = (int)(row_0['Calories'])
-        row_1 = data.iloc[p[1]]
+        row_1 = selection.iloc[p[1]]
         calories_1 = (int)(row_1['Calories'])
-        row_2 = data.iloc[p[2]]
+        row_2 = selection.iloc[p[2]]
         calories_2 = (int)(row_2['Calories'])
-        row_3 = data.iloc[p[3]]
+        row_3 = selection.iloc[p[3]]
         calories_3 = (int)(row_3['Calories'])
-        while nums[0]*calories_0 < calories:
-            nums[0] = nums[0]+1
-            while nums[0]*calories_0 + nums[1]*calories_1 < calories:
-                nums[1] = nums[1]+1
-                while nums[0]*calories_0 + nums[1]*calories_1 + nums[2]*calories_2< calories:
-                    nums[2] = nums[2]+1
-                    while nums[0]*calories_0 + nums[1]*calories_1 + nums[2]*calories_2 + nums[3]*calories_3 < calories:
-                        nums[3] = nums[3]+1
-                    cases = np.vstack((cases, nums))
-                    nums[3] = 0
-                cases = np.vstack((cases, nums))
-                nums[2] = 0
-            cases = np.vstack((cases, nums))
-            nums[1] = 0
-        cases = np.vstack((cases, nums))
-        nums[0] = 0
+
+        for i in range(20):
+            cases = np.vstack((cases, [np.random.randint(15, 35)/100*calories/calories_0,
+                                       np.random.randint(15, 35)/100*calories/calories_1,
+                                       np.random.randint(15, 35)/100*calories/calories_2,
+                                       np.random.randint(15, 35)/100*calories/calories_3]))
+
+        minimum = float('inf')
         oFat_0 = (float)(row_0['Fat'])
         oFat_1 = (float)(row_1['Fat'])
         oFat_2 = (float)(row_2['Fat'])
@@ -104,31 +137,31 @@ def suggested_meal(location, meal):
         oProtein_1 = (float)(row_1['Protein'])
         oProtein_2 = (float)(row_2['Protein'])
         oProtein_3 = (float)(row_3['Protein'])
-        for i in range(0, cases.shape[0]-1):
+
+        for i in range(21):
             oMatrix = np.array([oFat_0*cases[i, 0]+oFat_1*cases[i, 1]+oFat_2*cases[i, 2]+oFat_3*cases[i, 3],
-                               oCholesterol_0*cases[i, 0]+oCholesterol_1*cases[i, 1]+oCholesterol_2*cases[i, 2]+oCholesterol_3*cases[i, 3],
-                               oSodium_0*cases[i, 0]+oSodium_1*cases[i, 1]+oSodium_2*cases[i, 2]+oSodium_3*cases[i, 3],
-                               oCarbs_0*cases[i, 0]+oCarbs_1*cases[i, 1]+oCarbs_2*cases[i, 2]+oCarbs_3*cases[i, 3],
-                               oProtein_0*cases[i, 0]+oProtein_1*cases[i, 1]+oProtein_2*cases[i, 2]+oProtein_3*cases[i, 3]])
+                                oCholesterol_0*cases[i, 0]+oCholesterol_1*cases[i, 1]+oCholesterol_2*cases[i, 2]+oCholesterol_3*cases[i, 3],
+                                oSodium_0*cases[i, 0]+oSodium_1*cases[i, 1]+oSodium_2*cases[i, 2]+oSodium_3*cases[i, 3],
+                                oCarbs_0*cases[i, 0]+oCarbs_1*cases[i, 1]+oCarbs_2*cases[i, 2]+oCarbs_3*cases[i, 3],
+                                oProtein_0*cases[i, 0]+oProtein_1*cases[i, 1]+oProtein_2*cases[i, 2]+oProtein_3*cases[i, 3]])
             if distance.euclidean(tMatrix, oMatrix) < minimum:
                 minimum = distance.euclidean(tMatrix, oMatrix)
                 ideal = cases[i]
                 idealFoods = p
-
-    food1 = data.iloc[idealFoods[0]]
-    food2 = data.iloc[idealFoods[1]]
-    food3 = data.iloc[idealFoods[2]]
-    food4 = data.iloc[idealFoods[3]]
+    food1 = selection.iloc[idealFoods[0]]
+    food2 = selection.iloc[idealFoods[1]]
+    food3 = selection.iloc[idealFoods[2]]
+    food4 = selection.iloc[idealFoods[3]]
 
     label1 = food1['Label']
     label2 = food2['Label']
     label3 = food3['Label']
     label4 = food4['Label']
 
-    amount1 = (float)(re.findall(r'[\d\.\d]+', food1['Serving Size'])[0])*ideal[0]
-    amount2 = (float)(re.findall(r'[\d\.\d]+', food2['Serving Size'])[0])*ideal[1]
-    amount3 = (float)(re.findall(r'[\d\.\d]+', food3['Serving Size'])[0])*ideal[2]
-    amount4 = (float)(re.findall(r'[\d\.\d]+', food4['Serving Size'])[0])*ideal[3]
+    amount1 = np.round((float)(re.findall(r'[\d\.\d]+', food1['Serving Size'])[0])*ideal[0])
+    amount2 = np.round((float)(re.findall(r'[\d\.\d]+', food2['Serving Size'])[0])*ideal[1])
+    amount3 = np.round((float)(re.findall(r'[\d\.\d]+', food3['Serving Size'])[0])*ideal[2])
+    amount4 = np.round((float)(re.findall(r'[\d\.\d]+', food4['Serving Size'])[0])*ideal[3])
 
     unit1 = food1['Serving Size'].lstrip('0123456789.- ')
     unit2 = food2['Serving Size'].lstrip('0123456789.- ')
@@ -148,6 +181,31 @@ def suggested_meal(location, meal):
 def no_intent():
   no_message = "okay bye"
   return statement(no_message)
+
+@ask.intent("AMAZON.StopIntent")
+def stop_intent():
+  stop_message = "goodbye and enjoy your meals!"
+  return statement(stop_message)
+
+@ask.intent("AMAZON.HelpIntent")
+def help_intent():
+  help_message = "ask me for princeton dining hall meal suggestions."
+  return statement(help_message)
+
+@ask.intent("AMAZON.CancelIntent")
+def cancel_intent():
+  cancel_message = "cancelling my report."
+  return statement(cancel_message)
+
+@ask.intent("WigglesIntent")
+def wiggles_intent():
+  wiggles_message = "yummy yummy."
+  return statement(wiggles_message)
+
+@ask.intent("WhoopsIntent")
+def whoops_intent():
+    whoops_message = "fruit salad."
+    return statement(whoops_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
